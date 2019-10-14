a9dd06e51567f1359a7394e5044218aae60dd508
Lance Charles <lancechao_94@hotmail.com>
2019-10-14 17:02:08 +0800

    Login+Pofile+Fragment

--- app/src/androidTest/java/com/example/a3x03/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/a3x03/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.team09.hwealth;
+package com.example.a3x03;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.team09.hwealth", appContext.getPackageName());
+        assertEquals("com.example.a3x03", appContext.getPackageName());
     }
 }
