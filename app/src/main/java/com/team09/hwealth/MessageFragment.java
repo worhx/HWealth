bf48a158ba6898a84156de05464136eed11557f7
Adrian Foo <adrian_foo_94@hotmail.com>
2019-11-01 19:17:58 +0800

    messaging

--- app/src/main/java/com/team09/hwealth/MessageFragment.java
+++ app/src/main/java/com/team09/hwealth/MessageFragment.java
@@ -1,37 +1,154 @@
 package com.team09.hwealth;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MessageFragment extends Fragment {
+    private static final String CONVERSATION_URL = "https://hwealth.herokuapp.com/api/conversation";
+    private static final String SHAREDPREF = "SHAREDPREF";
+    private SharedPreferences prefs;
 
-    String[] nameArray = {"Adrian","Alan","Marcus","Jeremy",
-            "Beng","Jovan","Charles"};
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view =  inflater.inflate(R.layout.fragment_message,container,false);
+        prefs = Objects.requireNonNull(getActivity()).getSharedPreferences(SHAREDPREF, Context.MODE_PRIVATE);
+        JSONObject messageJSON = new JSONObject();
+        retrieveName(messageJSON, view);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(getContext(), R.layout.fragment_listview, nameArray);
-
-        ListView messageListView = view.findViewById(R.id.message_list);
-        messageListView.setAdapter(adapter);
-        messageListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MessageListFragment()).commit();
-            }
-        });
 
         return view;
     }
+
+    private void retrieveName(JSONObject data, View view){
+        final String saveData = data.toString();
+        final ArrayList<MessageData> names = new ArrayList<>();
+        final ListView messageListView = view.findViewById(R.id.message_list);
+        RequestQueue mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity().getApplicationContext()));
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, CONVERSATION_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            String cid = "";
+                            if (jsonResponse.getString("error").equals("false")){
+                                JSONArray jsonConvo = new JSONArray(jsonResponse.getString("allConversation"));
+
+                                for(int i = 0; i < jsonConvo.length(); i++){
+                                    MessageData data = new MessageData();
+                                    JSONObject convo = jsonConvo.getJSONObject(i);
+                                    cid = convo.getString("_id");
+                                    JSONArray members = convo.getJSONArray("members");
+                                    JSONObject name = members.getJSONObject(0);
+                                    data.setName(name.getJSONObject("accountId").get("username").toString());
+                                    data.setUid(name.getJSONObject("accountId").get("_id").toString());
+                                    data.setCid(cid);
+                                    names.add(data);
+                                }
+                            }
+
+                            String[] nameArray = new String[names.size()];
+                            for(int i = 0; i < names.size(); i++){
+                                nameArray[i] = names.get(i).getName();
+                            }
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.fragment_listview, nameArray);
+                            messageListView.setAdapter(adapter);
+                            messageListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    MessageListFragment mlf = new MessageListFragment();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,mlf).commit();
+                                    mlf.sendId(names.get(i).getUid(), names.get(i).getCid());
+                                }
+                            });
+                        } catch(JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String strJSONError = new String(networkResponse.data);
+                    JSONObject errorJSON;
+                    try {
+                        errorJSON = new JSONObject(strJSONError);
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                String iv = prefs.getString("keyIv", "null");
+                String encrypted = prefs.getString("encryptedKey", "");
+                try {
+                    Cryptor cryptor = new Cryptor();
+                    cryptor.initKeyStore();
+                    String decrypted = cryptor.decryptText(encrypted, iv);
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + decrypted);
+                    return headers;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public byte[] getBody() {
+                return saveData.getBytes(StandardCharsets.UTF_8);
+            }
+
+        };
+        mQueue.add(stringRequest);
+    }
+
 }
--- app/src/main/java/com/team09/hwealth/MessageListFragment.java
+++ app/src/main/java/com/team09/hwealth/MessageListFragment.java
@@ -1,47 +1,244 @@
 package com.team09.hwealth;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class MessageListFragment extends Fragment {
 
-
-    public MessageListFragment() {
-        // Required empty public constructor
-    }
+    private static final String MESSAGE_URL = "https://hwealth.herokuapp.com/api/message";
+    private static final String CONVO_URL = "https://hwealth.herokuapp.com/api/conversation";
+    private static final String SHAREDPREF = "SHAREDPREF";
+    private String uid;
+    private String cid;
+    private SharedPreferences prefs;
+    private RequestQueue mQueue;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        final View view =  inflater.inflate(R.layout.fragment_message_list, container, false);
+        final View topview =  inflater.inflate(R.layout.fragment_message_list, container, false);
+        Button msgBtn = topview.findViewById(R.id.button_chatbox_send);
+        final EditText chatbox = topview.findViewById(R.id.edittext_chatbox);
+        prefs = Objects.requireNonNull(getActivity()).getSharedPreferences(SHAREDPREF, Context.MODE_PRIVATE);
+        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity().getApplicationContext()));
 
-        List messageDataList = new ArrayList<MessageData>();
-        messageDataList.add(new MessageData("Adrian", "Hello", "10:30"));
-        messageDataList.add(new MessageData("Alan", "Hello", "10:31"));
-        messageDataList.add(new MessageData("Adrian", "I FAT", "10:32"));
-        messageDataList.add(new MessageData("Alan", "Go GYM", "10:33"));
+        msgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String messageToSend = chatbox.getText().toString();
 
-
-
-        ArrayAdapter adapter = new MessageAdapter(getContext(), messageDataList);
-        ListView listView = view.findViewById(R.id.listview_messages);
-        listView.setAdapter(adapter);
+                if(messageToSend.isEmpty()){
+                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "Please type in your message", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    JSONObject sendMsg = new JSONObject();
+                    try{
+                        sendMsg.put("recipient", uid);
+                        sendMsg.put("message", messageToSend);
+                    } catch(JSONException e){
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                    submitMsg(sendMsg, topview);
+                }
+            }
+        });
+        JSONObject messageJSON = new JSONObject();
+        getMsg(messageJSON, topview);
 
         // Inflate the layout for this fragment
-        return view;
+        return topview;
+    }
+
+    protected void sendId(String uid, String cid){
+        this.uid = uid;
+        this.cid = cid;
+    }
+
+    private void submitMsg(JSONObject data, final View view){
+        final String saveData = data.toString();
+        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, MESSAGE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    if (jsonResponse.getString("error").equals("false")){
+                        JSONObject messageJSON = new JSONObject();
+                        getMsg(messageJSON, view);
+                        EditText msgETxt = view.findViewById(R.id.edittext_chatbox);
+                        ListView lv = view.findViewById(R.id.listview_messages);
+                        msgETxt.setText("");
+
+
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String strJSONError = new String(networkResponse.data);
+                    JSONObject errorJSON;
+                    try {
+                        errorJSON = new JSONObject(strJSONError);
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }){
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                String iv = prefs.getString("keyIv", "null");
+                String encrypted = prefs.getString("encryptedKey", "");
+                try {
+                    Cryptor cryptor = new Cryptor();
+                    cryptor.initKeyStore();
+                    String decrypted = cryptor.decryptText(encrypted, iv);
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + decrypted);
+                    return headers;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public byte[] getBody() {
+                return saveData.getBytes(StandardCharsets.UTF_8);
+            }
+        };
+        mQueue.add(stringRequest);
+    }
+
+    private void getMsg(JSONObject data, View view){
+        final String saveData = data.toString();
+        final ArrayList<MessageData> msgArrList = new ArrayList<>();
+        final ListView listView = view.findViewById(R.id.listview_messages);
+        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, CONVO_URL + "/" + cid, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    if (jsonResponse.getString("error").equals("false")){
+                        JSONArray jsonMsgArr = new JSONArray(jsonResponse.getString("messages"));
+
+                        for(int i = 0; i < jsonMsgArr.length(); i++){
+                            MessageData data = new MessageData();
+                            JSONObject jsonMsg = jsonMsgArr.getJSONObject(i);
+                            data.setName(jsonMsg.getJSONObject("sentBy").getString("username"));
+                            data.setMessage(jsonMsg.getString("message"));
+                            String time = jsonMsg.getString("createdAt");
+                            data.setTime(time.substring(0,10) + "\n" + time.substring(11,19));
+                            msgArrList.add(data);
+                        }
+                        ArrayAdapter adapter = new MessageAdapter(getContext(), msgArrList);
+
+                        listView.setAdapter(adapter);
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String strJSONError = new String(networkResponse.data);
+                    JSONObject errorJSON;
+                    try {
+                        errorJSON = new JSONObject(strJSONError);
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }){
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                String iv = prefs.getString("keyIv", "null");
+                String encrypted = prefs.getString("encryptedKey", "");
+                try {
+                    Cryptor cryptor = new Cryptor();
+                    cryptor.initKeyStore();
+                    String decrypted = cryptor.decryptText(encrypted, iv);
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + decrypted);
+                    return headers;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public byte[] getBody() {
+                return saveData.getBytes(StandardCharsets.UTF_8);
+            }
+        };
+        mQueue.add(stringRequest);
     }
 
 }
