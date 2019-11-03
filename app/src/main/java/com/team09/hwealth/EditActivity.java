cca958e23452efe83019b13bf4351ac4675dafd5
Beng Hian Ker <benghianker@gmail.com>
2019-11-03 13:40:19 +0800

    Editing dob done
    Signed-off-by: Beng Hian Ker <benghianker@gmail.com>

--- app/src/main/java/com/team09/hwealth/EditActivity.java
+++ app/src/main/java/com/team09/hwealth/EditActivity.java
@@ -35,13 +35,17 @@ public class EditActivity extends AppCompatActivity {
     private static final String TAG = "EditActivity";
     private static final String UPDATE_BMI_URL = "https://hwealth.herokuapp.com/api/profile/update-bmi";
     private static final String UPDATE_EMAIL_URL = "https://hwealth.herokuapp.com/api/account/update-email";
+    private static final String UPDATE_DOB_URL = "https://hwealth.herokuapp.com/api/profile/update-profile";
     private static final String SHAREDPREF = "SHAREDPREF";
     private EditText bmi;
     private EditText height;
     private EditText weight;
     private TextView name;
     private EditText email;
+    private EditText dob;
+    private String intentDobStr;
     private String intentEmailStr;
+    private String intentNameStr;
     private RequestQueue mQueue;
     private SharedPreferences prefs;
 
@@ -57,10 +61,14 @@ public class EditActivity extends AppCompatActivity {
         name = findViewById(R.id.userTV);
         bmi = findViewById(R.id.bmiET);
         email = findViewById(R.id.emailET);
+        dob = findViewById(R.id.dobET);
+        dob.setText(getIntent().getStringExtra("dob"));
+        intentDobStr = getIntent().getStringExtra("dob");
         height.setText(getIntent().getStringExtra("height"));
         weight.setText(getIntent().getStringExtra("weight"));
         bmi.setText(getIntent().getStringExtra("bmi"));
         name.setText(getIntent().getStringExtra("name"));
+        intentNameStr = getIntent().getStringExtra("name");
         email.setText(getIntent().getStringExtra("email"));
         intentEmailStr = getIntent().getStringExtra("email");
         close.setOnClickListener(new View.OnClickListener() {
@@ -79,19 +87,44 @@ public class EditActivity extends AppCompatActivity {
                         if (weight.getText().toString().matches("\\b[1-9]\\d{0,2}\\.\\d{0,2}\\b")) {
                             JSONObject bmiJSON = new JSONObject();
                             JSONObject emailJSON = new JSONObject();
+                            JSONObject dobJSON = new JSONObject();
                             final EditText weightET = findViewById(R.id.weightET);
                             final EditText heightET = findViewById(R.id.heightET);
                             final EditText emailET = findViewById(R.id.emailET);
+                            final EditText dobET = findViewById(R.id.dobET);
                             try {
                                 bmiJSON.put("weight", weightET.getText().toString());
                                 bmiJSON.put("height", heightET.getText().toString());
-                                emailJSON.put("email", emailET.getText().toString());
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                             if (!emailET.getText().toString().equals(intentEmailStr)) {
                                 if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
-                                    SubmitEmail(emailJSON);
+                                    try {
+                                        emailJSON.put("email", emailET.getText().toString());
+                                        SubmitEmail(emailJSON);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Please enter email in the format example@gmail.com", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                            } else if (!dobET.getText().toString().equals(intentDobStr)) {
+                                if (dobET.getText().toString().matches("^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$")) {
+                                    try {
+                                        dobJSON.put("dateOfBirth", dobET.getText().toString());
+                                        dobJSON.put("fullname", intentNameStr);
+                                        SubmitDOB(dobJSON);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Please enter date in the format 1992-12-19", Toast.LENGTH_LONG).show();
+                                    return;
                                 }
                             }
                             SubmitBMI(bmiJSON);
@@ -203,6 +236,78 @@ public class EditActivity extends AppCompatActivity {
                                 Log.d(TAG, jsonResponse.getString("message"));
 
                             }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String strJSONError = new String(networkResponse.data);
+                    JSONObject errorJSON;
+                    try {
+                        errorJSON = new JSONObject(strJSONError);
+                        Toast.makeText(getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                String iv = prefs.getString("keyIv", "null");
+                String encrypted = prefs.getString("encryptedKey", "");
+                try {
+                    Cryptor cryptor = new Cryptor();
+                    cryptor.initKeyStore();
+                    String decrypted = cryptor.decryptText(encrypted, iv);
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + decrypted);
+                    return headers;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+
+            @Override
+            public byte[] getBody() {
+                return saveData.getBytes(StandardCharsets.UTF_8);
+            }
+
+        };
+        mQueue.add(stringRequest);
+    }
+
+    private void SubmitDOB(JSONObject data) {
+        final String saveData = data.toString();
+        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getApplicationContext()));
+
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT, UPDATE_DOB_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse.getString("error").equals("false")) {
+                                Log.d(TAG, jsonResponse.getString("message"));
+
+                            }
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
