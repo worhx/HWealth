94191dc24875f872de49395ef949c89b0e4044da
Beng Hian Ker <benghianker@gmail.com>
2019-11-04 15:28:19 +0800

    Added function to create steps
    Signed-off-by: Beng Hian Ker <benghianker@gmail.com>

--- app/src/main/java/com/team09/hwealth/StepsFragment.java
+++ app/src/main/java/com/team09/hwealth/StepsFragment.java
@@ -7,10 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -32,7 +30,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -49,13 +46,45 @@ public class StepsFragment extends Fragment {
     private SharedPreferences prefs;
     private ArrayList<String> mDate = new ArrayList<>();
     private ArrayList<String> mStep = new ArrayList<>();
+    private String date;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_steps, container, false);
         prefs = Objects.requireNonNull(getActivity()).getSharedPreferences(SHAREDPREF, Context.MODE_PRIVATE);
         JSONObject send = new JSONObject();
-        RetrieveSteps(send,view);
+        RetrieveSteps(send, view);
+        date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        Button createSteps = view.findViewById(R.id.createStepsButton);
+        final EditText stepsET = view.findViewById(R.id.stepsET);
+
+        createSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!stepsET.getText().toString().equals("")) {
+                    if (stepsET.getText().toString().matches("^[0-9]{1,6}$")) {
+                        JSONObject send = new JSONObject();
+                        int steps = Integer.parseInt(stepsET.getText().toString());
+                        try {
+                            send.put("totalSteps", steps);
+                            send.put("dateRecorded", date);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, send.toString());
+                        stepsET.setText("");
+                        SubmitSteps(send);
+                    } else {
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "Please enter number within 6 digits", Toast.LENGTH_LONG).show();
+
+                    }
+                } else {
+                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "Please enter steps", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
         return view;
     }
 
@@ -71,18 +100,18 @@ public class StepsFragment extends Fragment {
                             if (jsonResponse.getString("error").equals("false")) {
                                 Log.d(TAG, jsonResponse.toString());
                                 JSONArray recordsJSONArr = new JSONArray(jsonResponse.getString("records"));
-                                for(int i = 0;i<recordsJSONArr.length();i++){
+                                for (int i = 0; i < recordsJSONArr.length(); i++) {
                                     JSONObject recordsJSONArrJSONObject = recordsJSONArr.getJSONObject(i);
                                     String a = recordsJSONArrJSONObject.getString("totalSteps");
-                                    Log.d(TAG,a);
+                                    Log.d(TAG, a);
                                     String b = recordsJSONArrJSONObject.getString("dateRecorded");
-                                    mDate.add(b.substring(0,10));
-                                    Log.d(TAG,b);
-                                    Log.d(TAG,"END");
+                                    mDate.add(b.substring(0, 10));
+                                    Log.d(TAG, b);
+                                    Log.d(TAG, "END");
                                     mStep.add(a);
                                 }
-                                Log.d(TAG,mStep.toString());
-                                Log.d(TAG,mDate.toString());
+                                Log.d(TAG, mStep.toString());
+                                Log.d(TAG, mDate.toString());
                                 initRecyclerView(view);
                             }
 
@@ -136,86 +165,83 @@ public class StepsFragment extends Fragment {
         };
         mQueue.add(stringRequest);
     }
-    private void initRecyclerView(View view){
+
+    private void initRecyclerView(View view) {
         RecyclerView recyclerView = view.findViewById(R.id.recylerView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mStep,mDate,getActivity());
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mStep, mDate, getActivity());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
-//    /////////////////////////////
-//
-//    private void SubmitSteps(JSONObject data, View view) {
-//        final String saveData = data.toString();
-//        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
-//        final EditText currentStepsET = view.findViewById(R.id.currentStepsET);
-//
-//        StringRequest stringRequest = new StringRequest(Request.Method.POST, STEP_URL,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            JSONObject jsonResponse = new JSONObject(response);
-//                            if (jsonResponse.getString("error").equals("false")) {
-//                                Log.d(TAG, jsonResponse.toString());
-//                                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), jsonResponse.getString("message"), Toast.LENGTH_LONG).show();
-//                                currentStepsET.setText(jsonResponse.getString("message"));
-//                            }
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                NetworkResponse networkResponse = error.networkResponse;
-//                if (networkResponse != null && networkResponse.data != null) {
-//                    String strJSONError = new String(networkResponse.data);
-//                    JSONObject errorJSON;
-//                    try {
-//                        errorJSON = new JSONObject(strJSONError);
-//                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
-//                    } catch (JSONException e) {
-//                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-//                    }
-//
-//                }
-//            }
-//        }) {
-//            @Override
-//            public String getBodyContentType() {
-//                return "application/json; charset=utf-8";
-//            }
-//
-//            @Override
-//            public Map<String, String> getHeaders() {
-//
-//                String iv = prefs.getString("keyIv", "null");
-//                String encrypted = prefs.getString("encryptedKey", "");
-//                try {
-//                    Cryptor cryptor = new Cryptor();
-//                    cryptor.initKeyStore();
-//                    String decrypted = cryptor.decryptText(encrypted, iv);
-//                    HashMap<String, String> headers = new HashMap<>();
-//                    headers.put("Authorization", "Bearer " + decrypted);
-//                    return headers;
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                return null;
-//            }
-//
-//
-//            @Override
-//            public byte[] getBody() {
-//                return saveData.getBytes(StandardCharsets.UTF_8);
-//            }
-//
-//        };
-//        mQueue.add(stringRequest);
-//    }
-//    ///////////////////////////
+    private void SubmitSteps(JSONObject data) {
+        final String saveData = data.toString();
+        mQueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, STEP_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse.getString("error").equals("false")) {
+                                Log.d(TAG, jsonResponse.toString());
+                                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), jsonResponse.getString("message"), Toast.LENGTH_LONG).show();
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String strJSONError = new String(networkResponse.data);
+                    JSONObject errorJSON;
+                    try {
+                        errorJSON = new JSONObject(strJSONError);
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), errorJSON.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+
+                String iv = prefs.getString("keyIv", "null");
+                String encrypted = prefs.getString("encryptedKey", "");
+                try {
+                    Cryptor cryptor = new Cryptor();
+                    cryptor.initKeyStore();
+                    String decrypted = cryptor.decryptText(encrypted, iv);
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + decrypted);
+                    return headers;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+
+            @Override
+            public byte[] getBody() {
+                return saveData.getBytes(StandardCharsets.UTF_8);
+            }
+
+        };
+        mQueue.add(stringRequest);
+    }
+
 }
